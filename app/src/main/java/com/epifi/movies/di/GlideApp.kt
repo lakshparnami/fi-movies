96ee3d4f4390029656decc95e75ac067449e02a1
Lakshay <lakshay@godutchpay.in>
2022-09-03 22:55:27 +0530

    add glide cache

--- app/src/main/java/com/epifi/movies/di/GlideApp.kt
+++ app/src/main/java/com/epifi/movies/di/GlideApp.kt
@@ -1,6 +1,21 @@
 package com.epifi.movies.di
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.module.AppGlideModule
 
+
 @GlideModule
-class MyGlideApp : AppGlideModule()
+class MyGlideApp : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val cacheSize200MB = 209715200
+
+        builder.setDiskCache(
+            InternalCacheDiskCacheFactory(context, cacheSize200MB.toLong())
+        )
+
+        super.applyOptions(context, builder)
+    }
+}
